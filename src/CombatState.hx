831f72f22bb583debb8c8e80d114b13715a4eedc
MintPaw <jerusanders@gmail.com>
2016-04-05 18:40:36 -0600

    Added tiled

--- src/CombatState.hx
+++ src/CombatState.hx
@@ -5,32 +5,24 @@ import flixel.tile.*;
 import flixel.math.*;
 import flixel.graphics.*;
 import flixel.graphics.frames.*;
+import flixel.addons.editors.tiled.*;
 import openfl.*;
 
 class CombatState extends FlxState
 {
 	private var _tilemap:FlxTilemap;
-	
+
 	public function new() {
 		super();
 	}
 
 	override public function create():Void {
-		var mapWidth:Int = 32;
-		var mapHeight:Int = 32;
-
-		var mapArray:Array<Array<Int>> = [];
-		for (i in 0...mapHeight) {
-			var tmp:Array<Int> = [];
-			for (j in 0...mapWidth) tmp.push(0);
-			mapArray.push(tmp);
-		}
-
+		var tiledMap:TiledMap = new TiledMap("assets/map/test.tmx");
 		var tileGraphic:FlxTileFrames =
 			FlxTileFrames.fromRectangle("assets/img/tilemap.png", new FlxPoint(32, 32), null, new FlxPoint(5, 5));
 
 		_tilemap = new FlxTilemap();
-		_tilemap.loadMapFrom2DArray(mapArray, tileGraphic, 32, 32, null, 1);
+		_tilemap.loadMapFromCSV(cast(tiledMap.getLayer("main"), TiledTileLayer).csvData, tileGraphic, 32, 32, null, 1);
 		add(_tilemap);
 	}
 }
