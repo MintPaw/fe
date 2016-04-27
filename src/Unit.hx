0eefab0a56c9ff30940c6229c73274ee7ed8e0e0
MintPaw <jerusanders@gmail.com>
2016-04-27 14:34:29 -0600

    Added AP bar.

--- src/Unit.hx
+++ src/Unit.hx
@@ -5,6 +5,9 @@ import flixel.tile.*;
 import flixel.group.*;
 import flixel.math.*;
 import flixel.tweens.*;
+import flixel.ui.*;
+import flixel.ui.FlxBar;
+import flixel.util.*;
 import openfl.*;
 import Level;
 
@@ -13,6 +16,7 @@ class Unit extends FlxSpriteGroup
 	public static var UNIT_ID_COUNTER:Int = 0;
 
 	public var mainGraphic:FlxSprite = new FlxSprite();
+	public var apBar:FlxBar;
 
 	public var items:Array<Item> = [];
 	public var drops:Array<Item> = [];
@@ -26,12 +30,16 @@ class Unit extends FlxSpriteGroup
 	public var dead:Bool = false;
 	public var attackable:Bool = true;
 	public var controllable:Bool = false;
+	public var state:String = "default";
 
 	public function new() {
 		super();
+		apBar = new FlxBar(0, 0, FlxBarFillDirection.LEFT_TO_RIGHT, Reg.TILE_SIZE, 4, this, "ap", 0, 10, true);
+		apBar.visible = false;
 
 		mainGraphic.makeGraphic(32, 32, 0xFFFF0000);
 		add(mainGraphic);
+		add(apBar);
 	}
 
 	public function warp(x:Int, y:Int):Void {
@@ -41,18 +49,21 @@ class Unit extends FlxSpriteGroup
 	}
 
 	public function walk(path:Path):Float {
+		state = "walking";
 		var moveTime:Float = 0.1;
 		var delayTime:Float = 0.2;
 
 		for (i in 0...path.path.length) {
 			FlxTween.tween(this, {x:path.path[i].x * Reg.TILE_SIZE, y:path.path[i].y * Reg.TILE_SIZE}, moveTime,
-					{startDelay:delayTime*i, onComplete: i == path.path.length-1 ?
-						function(t:FlxTween):Void{ap -= path.costs[i]; reloadLoc();} : 
-						function(t:FlxTween):Void{ap -= path.costs[i];}
+					{startDelay:delayTime*i, onComplete:
+						function(t:FlxTween):Void{ap -= path.costs[i]; reloadLoc();}
 					});
 			}
 
-		return delayTime*path.path.length;
+		var timeWillTake:Float = delayTime*path.path.length;
+		new FlxTimer().start(timeWillTake, function(t:FlxTimer){state="default";});
+
+		return timeWillTake;
 	}
 
 	public function getNewItem(id:Int):Void {
@@ -66,6 +77,18 @@ class Unit extends FlxSpriteGroup
 
 	private function reloadLoc():Void {
 		location.set(Std.int(x / Reg.TILE_SIZE), Std.int(y / Reg.TILE_SIZE));
+	}
+
+	override public function update(elapsed:Float):Void {
+		if (state == "walking") {
+			apBar.x = mainGraphic.x + mainGraphic.width/2 - apBar.width/2;
+			apBar.y = mainGraphic.y + mainGraphic.height + apBar.height + 5;
+			apBar.visible = true;
+		} else {
+			apBar.visible = false;
+		}
+
+		super.update(elapsed);
 	}
 }
 
